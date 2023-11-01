c48ab6df492e2660b452f5bc015b330aa1f6d57f
mobus <sv0220@163.com>
2023-11-01 11:38:21 +0800

    update export client interface

--- client/interface.go
+++ client/interface.go
@@ -3,6 +3,7 @@ package client
 type Client interface {
 	Get(key []byte) ([]byte, error)
 	Put(key, value []byte) error
+	Delete(key []byte) error
 	Close() error
 	Has(key []byte) (bool, error)
 	NewBatch() (*Batch, error)
